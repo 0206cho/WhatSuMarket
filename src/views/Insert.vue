1642a2156f27bd09e9e6c27a9ad0b5555bda4e76
minsol <0206cho@gmail.com>
2023-01-31 10:57:14 +0900

    민솔 - 1차 점검 페이지 연결

--- src/views/Insert.vue
+++ src/views/Insert.vue
@@ -1,85 +1,55 @@
 <template>
-    <!-- 템플릿안에 컨테이너가 있어야함 -->
+  <!-- 템플릿안에 컨테이너가 있어야함 -->
 
-    <v-container fluid  fill-height>
-        <v-row justify="center" align="cener">
-            <v-col cols="6">
-                <v-card>
-                    <v-card-title>
-                        상품등록 
-                    </v-card-title>
-                    <v-simple-table height="300px">
-                        <template v-slot:default>
-                          <thead>
-                            <tr>
-                              <th class="text-left">
-                                Name
-                              </th>
-                              <th class="text-left">
-                                Calories
-                              </th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr
-                              v-for="item in desserts"
-                              :key="item.name"
-                            >
-                              <td>{{ item.name }}</td>
-                              <td>{{ item.calories }}</td>
-                            </tr>
-                          </tbody>
-                        </template>
-                      </v-simple-table>
-                </v-card>
-            </v-col>
-        </v-row>
-    
-    </v-container>
+  <v-container fluid  fill-height>
+      <v-row justify="center" align="cener">
+          <v-col cols="6">
+              <v-card>
+                  <v-card-title>
+                      상품등록
+                  </v-card-title>
+                  <v-card-text>
+                      <v-file-input :rules="rules" accept="image/png, image/jpeg, image/bmp" prepend-icon="mdi-camera" label="사진등록"></v-file-input>
+                      <v-text-field label="상품명" single-line></v-text-field>
+                      <v-text-field label="가격" single-line></v-text-field>
+                      <v-textarea name="input-7-1" label="상세설명" value="" hint=""></v-textarea>
+                  </v-card-text>
+                  <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn>등록</v-btn>
+                  </v-card-actions>
+              </v-card>
+          </v-col>
+      </v-row>
+  
+  </v-container>
 </template>
 <script>
 export default {
-    name: "product",
-
-    data () {
-      return {
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-        ],
-      }
-    },
-
-    
+  name: "product",
 
 
-    beforeCreate() {
-        console.log('beforeCreate')
-    },
-    created() {
-        console.log('created')
-    },
-    beforeMount() {
-        console.log('beforeMount')
-    },
-    mounted() {
-        console.log('mounted')
-    },
-    beforeDestroyed() {
-        console.log('beforeDestroyed')
-    },
-    destroyed() {
-        console.log('destroyed')
-    },
+  beforeCreate() {
+      console.log('beforeCreate')
+  },
+  created() {
+      console.log('created')
+  },
+  beforeMount() {
+      console.log('beforeMount')
+  },
+  mounted() {
+      console.log('mounted')
+  },
+  beforeDestroyed() {
+      console.log('beforeDestroyed')
+  },
+  destroyed() {
+      console.log('destroyed')
+  },
 }
 </script>
-       
+     
 <style>
 
 </style>
