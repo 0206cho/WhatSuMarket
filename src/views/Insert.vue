2c2b2309d8e3a18261dbe8f9baf46e16d0ada2fd
accforaus <accforaus@gmail.com>
2023-02-06 15:43:39 +0900

    modify: modify sell price panel title and layout for mobile responsibility

--- src/views/Insert.vue
+++ src/views/Insert.vue
@@ -1,56 +1,60 @@
 <template>
   <!-- 템플릿안에 컨테이너가 있어야함 -->
 
-  <v-container style="max-width:1300px"  fill-height>
-    <v-layout align-center row wrap>
-      <v-row justify="center" align="center">
-          <v-col cols="4">
-            <v-card elevation="12" width="550" height="100%"
-            style="border-radius:25px; border: 1px solid #008037">
-            <v-img :src="require('../../images/WhatSuWhite.png')" max-height="110" max-width="110"
-                style="margin: 1em auto;"></v-img>
-                <div style="text-align: center; color:#008037; font-size:xx-large; font-family: 'KOTRA_BOLD';" class="mt-0">상품등록</div>
-                  <v-card-text>
-                      <v-file-input :rules="rules" accept="image/png, image/jpeg, image/bmp" color="#008037" prepend-icon="mdi-camera" label="사진등록"></v-file-input>
-                      <v-text-field color="#008037" label="상품명" single-line></v-text-field>
-                      <v-text-field color="#008037" label="가격" single-line></v-text-field>
-                      <v-textarea color="#008037" name="input-7-1" label="상세설명" value="" hint=""></v-textarea>
-                  </v-card-text>
-                  <v-card-actions class="mx-2 mb-5">
-                    <v-spacer></v-spacer>
-                        <v-btn dark color="#008037" style="border-radius: 30px;" width="50%">확인</v-btn>
-                        <v-btn dark color="#6B8A78" style="border-radius: 30px;" width="50%">취소</v-btn>
-                        <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-          </v-col>
-      </v-row>
-    </v-layout>   
+  <v-container fluid fill-height style="height: 100%;">
+    <v-row justify="center" align="center">
+      <v-col cols="3" md="3" xs="12" sm="12">
+        <v-card elevation="12" height="100%"
+                style="border-radius:25px; border: 1px solid #008037">
+          <v-img :src="require('../../images/WhatSuWhite.png')" max-height="110" max-width="110"
+                 style="margin: 1em auto;"></v-img>
+          <div style="text-align: center; color:#008037; font-size:xx-large; font-family: 'KOTRA_BOLD';" class="mt-0">판매 신청</div>
+          <v-card-text>
+            <v-file-input v-model="files" chips multiple accept="image/png, image/jpeg, image/bmp" color="#008037" prepend-icon="mdi-camera" label="사진등록"></v-file-input>
+            <v-text-field v-model="sell_product.title" color="#008037" label="상품명" single-line></v-text-field>
+            <v-text-field v-model="sell_product.price" type="number" color="#008037" label="가격" single-line></v-text-field>
+            <v-textarea  v-model="sell_product.description" color="#008037" name="input-7-1" label="상세설명" value="" hint=""></v-textarea>
+          </v-card-text>
+          <v-card-actions class="mx-2 mb-5">
+            <v-spacer></v-spacer>
+            <v-btn dark color="#008037" style="border-radius: 30px;" width="50%" @click="onClickSend">확인</v-btn>
+            <v-btn dark color="#6B8A78" style="border-radius: 30px;" width="50%" @click="onClickCancel">취소</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
 export default {
   name: "Insert",
-
-
-  beforeCreate() {
-      console.log('beforeCreate')
-  },
-  created() {
-      console.log('created')
-  },
-  beforeMount() {
-      console.log('beforeMount')
-  },
-  mounted() {
-      console.log('mounted')
-  },
-  beforeDestroyed() {
-      console.log('beforeDestroyed')
-  },
-  destroyed() {
-      console.log('destroyed')
-  },
+  data: () => ({
+    sell_product: {
+      title: '',
+      description: '',
+      price: 0
+    },
+    files: null
+  }),
+  methods: {
+    onClickSend: function () {
+      const formData = new FormData()
+      for (let i = 0; i < this.files.length; i++) {
+        formData.append('files', this.files[i])
+      }
+      formData.append('request', new Blob([JSON.stringify(this.sell_product)]))
+      console.dir(formData)
+    },
+    onClickCancel: function () {
+      this.sell_product = {
+        title: '',
+        description: '',
+        price: 0
+      }
+      this.files = null
+    }
+  }
 }
 </script>
      
--- src/views/List.vue
+++ src/views/List.vue
@@ -3,7 +3,7 @@
     <h1 class="py-12"
         style="display: flex; justify-content: center; font-weight:bold; color:#008037; font-family:'KOTRA_BOLD';">사던지
       말던지</h1> <br>
-    <v-data-iterator :items="products" :items-per-page="6" hide-default-footer>
+    <v-data-iterator :items="products" :items-per-page="24" hide-default-footer>
       <template v-slot:default="props">
         <v-row>
           <v-col cols="4" v-for="(product, index) in props.items" :key="index" class="mb-3">
@@ -26,30 +26,6 @@
         </v-row>
       </template>
     </v-data-iterator>
-    <v-row style="display: flex; justify-content: center;" class="pb-8">
-      <v-col cols="3" class="mx-4" v-for="(product, index) in products" :key="index">
-        <v-hover v-slot="{ hover }">
-          <v-card @click="detail" :elevation="hover ? 16 : 2" class="mx-auto rounded-lg" width="300" height="320"
-                  max-height="320" style="border-radius:25px; border: 1px solid #008037">
-            <v-img height="200" :src="`https://picsum.photos/${index}00/300`"></v-img>
-
-            <v-card-title class="pb-1">{{ product.title }}</v-card-title>
-
-            <v-card-text class="pb-1">
-              <div class="black--text" style="font-weight: bold;">
-                {{ product.price.toLocaleString() }}원
-              </div>
-
-              <div class="pt-3" style="color:#008037">관심 {{ product.favorite }} &nbsp;| &nbsp; 채팅 {{
-                  product.chat
-                }}
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-hover>
-      </v-col>
-    </v-row>
-
   </v-container>
 </template>
 
